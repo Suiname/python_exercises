7a83bc32a223904b9dd4057272d1a838572c12cb
Jason Tham <jason.tham@tempus.com>
2016-08-18 10:23:03 -0500

    practice with file manipulation

---
+++ gc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+dna = 'acaaagcgtgcgatcgtacgtacgatcgatcactgatcgatc'
+no_c = dna.count('c')
+no_g = dna.count('g')
+dna_length = len(dna)
+gc_percent = (no_c + no_g) * 100.0 / dna_length
+print(gc_percent)
